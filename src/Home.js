dc6adf64df2ebf6c283184dbb5f914ef58ab9193
16Aquagirl <ritikaphilip2000@gmail.com>
2024-02-23 16:56:57 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -32,27 +32,27 @@ const Home = () => {
                     <div className="marquee">
                         <div className="marquee-content">
                             <div className="marquee-item">
-                            <img className="logo1" src={enfieldlogo}/>
+                                <img className="logo1" src={enfieldlogo} />
                             </div>
 
                             <div className="marquee-item">
-                            <img className="logo2"  src={bmwlogo}/>
+                                <img className="logo2" src={bmwlogo} />
                             </div>
 
                             <div className="marquee-item">
-                            <img className="logo3" src={jawalogo}/>
+                                <img className="logo3" src={jawalogo} />
                             </div>
 
                             <div className="marquee-item">
-                            <img className="logo4" src={ktmlogo}/>
+                                <img className="logo4" src={ktmlogo} />
                             </div>
 
                             <div className="marquee-item">
-                            <img className="logo5"  src={olalogo}/>
+                                <img className="logo5" src={olalogo} />
                             </div>
 
                             <div className="marquee-item">
-                            <img className="logo6" src={harleylogo}/>
+                                <img className="logo6" src={harleylogo} />
                             </div>
                         </div>
                     </div>
@@ -97,7 +97,7 @@ const Home = () => {
                             {/* <!-- first card --> */}
                             <div class="card-item">
                                 {/* <img src="{% static 'royalenfieldhunter350.webp' %}" width="300px"></img> */}
-                                <img className="product" src={royal}/>
+                                <img className="product" src={royal} />
                                 <div class="detail">
                                     <h2>Royal Enfield Hunter</h2>
                                     <p>1,50,000-1,75,000</p>
@@ -108,7 +108,7 @@ const Home = () => {
                             {/* <!-- second card --> */}
                             <div class="card-item">
                                 {/* <img src="{% static 'ktmrc200.webp' %}" width="300px"></img> */}
-                                <img className="product" src={ktm}/>
+                                <img className="product" src={ktm} />
                                 <div class="detail">
                                     <h2>KTM Rc 200</h2>
                                     <p>2,18,000</p>
@@ -118,7 +118,7 @@ const Home = () => {
                             {/* <!-- third card --> */}
                             <div class="card-item">
                                 {/* <img src="{% static 'jawaperak.webp' %}" width="300px"></img> */}
-                                <img className="product" src={jawa}/>
+                                <img className="product" src={jawa} />
                                 <div class="detail">
                                     <h2>Jawa Perak</h2>
                                     <p>2,14,000</p>
@@ -186,66 +186,62 @@ const Home = () => {
                 {/* footer */}
                 <div className="footer">
                     <footer className="new_footer_area bg_color">
-                        <div className="new_footer_top">
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-lg-3 col-md-6">
-                                        <div className="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" style={{ visibility: 'visible', animationDelay: '0.2s', animationName: 'fadeInLeft' }}>
-                                            <h3 className="f-title f_600 t_color f_size_18">Get in Touch</h3>
-                                            <p>Don’t miss any updates of our new templates and extensions.!</p>
-                                            <form action="#" className="f_subscribe_two mailchimp" method="post" noValidate="true" _lpchecked={1}>
-                                                <input type="text" name="EMAIL" className="form-control memail" placeholder="Email" />
-                                                <button className="btn btn_get btn_get_two" type="submit">Subscribe</button>
-                                                <p className="mchimp-errmessage" style={{ display: 'none' }} />
-                                                <p className="mchimp-sucmessage" style={{ display: 'none' }} />
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-6">
-                                        <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.4s" style={{ visibility: 'visible', animationDelay: '0.4s', animationName: 'fadeInLeft' }}>
-                                            <h3 className="f-title f_600 t_color f_size_18">Download</h3>
-                                            <ul className="list-unstyled f_list">
-                                                <li><a href="#">Company</a></li>
-                                                <li><a href="#">Android App</a></li>
-                                                <li><a href="#">ios App</a></li>
-                                                <li><a href="#">Desktop</a></li>
-                                                <li><a href="#">Projects</a></li>
-                                                <li><a href="#">My tasks</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-6">
-                                        <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0.6s', animationName: 'fadeInLeft' }}>
-                                            <h3 className="f-title f_600 t_color f_size_18">Help</h3>
-                                            <ul className="list-unstyled f_list">
-                                                <li><a href="#">FAQ</a></li>
-                                                <li><a href="#">Term &amp; conditions</a></li>
-                                                <li><a href="#">Reporting</a></li>
-                                                <li><a href="#">Documentation</a></li>
-                                                <li><a href="#">Support Policy</a></li>
-                                                <li><a href="#">Privacy</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-3 col-md-6">
-                                        <div className="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style={{ visibility: 'visible', animationDelay: '0.8s', animationName: 'fadeInLeft' }}>
-                                            <h3 className="f-title f_600 t_color f_size_18">Team Solutions</h3>
-                                            <div className="f_social_icon">
-                                                <a href="#" className="f_social_icon" ><img className="socialicon1" src={whatsapp}/></a>
-                                                <a href="#" ><img className="socialicon2" src={ig}/></a>
-                                                <a href="#" ><img className="socialicon3" src={fb}/></a>
-                                                <a href="#" ><img className="socialicon4" src={twitter}/></a>
+                    <div className="new_footer_top">
+                            <div className="footer_bg">
+                            <div className="footer_bg_one">
+                                <div className="footer_bg_two">
+                                </div>
+                            </div>
+                                        <div className="container">
+                                            <div className="row">
+                                                <div className="col-lg-3 col-md-6">
+                                                    <div className="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" style={{ visibility: 'visible', animationDelay: '0.2s', animationName: 'fadeInLeft' }}>
+                                                        <h3 className="f-title f_600 t_color f_size_18">Get in Touch</h3>
+                                                        <p>Don’t miss any updates of our new templates and extensions.!</p>
+                                                        <form action="#" className="f_subscribe_two mailchimp" method="post" noValidate="true" _lpchecked={1}>
+                                                            <input type="text" name="EMAIL" className="form-control memail" placeholder="Email" />
+                                                            <button className="btn btn_get btn_get_two" type="submit">Subscribe</button>
+                                                            <p className="mchimp-errmessage" style={{ display: 'none' }} />
+                                                            <p className="mchimp-sucmessage" style={{ display: 'none' }} />
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                                <div className="col-lg-3 col-md-6">
+                                                    <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.4s" style={{ visibility: 'visible', animationDelay: '0.4s', animationName: 'fadeInLeft' }}>
+                                                        <h3 className="f-title f_600 t_color f_size_18">BIKES</h3>
+                                                        <ul className="list-unstyled f_list">
+                                                            <li><a href="#">Royal Enfield</a></li>
+                                                            <li><a href="#">KTM</a></li>
+                                                            <li><a href="#">Harley Davidson</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div className="col-lg-3 col-md-6">
+                                                    <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0.6s', animationName: 'fadeInLeft' }}>
+                                                        <h3 className="f-title f_600 t_color f_size_18">ELECTRIC BIKES</h3>
+                                                        <ul className="list-unstyled f_list">
+                                                            <li><a href="#">OLA</a></li>
+                                                            <li><a href="#">Pure EV</a></li>
+                                                            <li><a href="#">Bmw</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div className="col-lg-3 col-md-6">
+                                                    <div className="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style={{ visibility: 'visible', animationDelay: '0.8s', animationName: 'fadeInLeft' }}>
+                                                        <h3 className="f-title f_600 t_color f_size_18">Team Solutions</h3>
+                                                        <div className="f_social_icon">
+                                                            <a href="#" className="f_social_icon" ><img className="socialicon1" src={whatsapp} /></a>
+                                                            <a href="#" ><img className="socialicon2" src={ig} /></a>
+                                                            <a href="#" ><img className="socialicon3" src={fb} /></a>
+                                                            <a href="#" ><img className="socialicon4" src={twitter} /></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="footer_bg">
-                                <div className="footer_bg_one" />
-                                <div className="footer_bg_two" />
-                            </div>
+                               
                         </div>
-
                     </footer>
 
                 </div>
